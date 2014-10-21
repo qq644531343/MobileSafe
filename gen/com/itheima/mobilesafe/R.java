6015a0506abb5ba495407b1b0043c1a1f9f0f621
libo_liu <qq644531343@gmail.com>
2014-10-21 15:45:54 +0800

    增加启动页及升级检测

--- gen/com/itheima/mobilesafe/R.java
+++ gen/com/itheima/mobilesafe/R.java
@@ -1078,6 +1078,7 @@ containing a value of this type.
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
         public static final int ic_launcher=0x7f020057;
+        public static final int luncher_bg=0x7f020058;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1091,7 +1092,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1119,6 +1120,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int rl_root_splash=0x7f05003d;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1136,8 +1138,10 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f05003c;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
+        public static final int tv_splash_version=0x7f05003e;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1192,8 +1196,9 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_splash=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_home=0x7f030018;
+        public static final int activity_splash=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int splash=0x7f0c0000;
@@ -1239,9 +1244,10 @@ containing a value of this type.
         /**  Description of a share target (both in the list of such or the default share button) in a ShareActionProvider (share UI). [CHAR LIMIT=NONE] 
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
-        public static final int action_settings=0x7f0a000f;
-        public static final int app_name=0x7f0a000d;
-        public static final int hello_world=0x7f0a000e;
+        public static final int action_settings=0x7f0a0010;
+        public static final int app_name=0x7f0a000e;
+        public static final int appversion=0x7f0a000f;
+        public static final int serverurl=0x7f0a000d;
     }
     public static final class style {
         /** 
--- src/com/itheima/mobilesafe/SplashActivity.java
+++ src/com/itheima/mobilesafe/SplashActivity.java
@@ -1,34 +1,195 @@
+/**
+ * 	功能：
+ * 	1，显示logo及启动页
+ * 2，应用程序初始化
+ * 3，检查版本信息
+ * 4，检查版权
+ */
+
 package com.itheima.mobilesafe;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-public class SplashActivity extends ActionBarActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.itheima.mobilesafe.utils.StreamTools;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.animation.AlphaAnimation;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SplashActivity extends Activity {
+	
+	protected static final String TAG = "SplashActivity";
+	
+	protected static final int ENTER_HOME = 1;
+	protected static final int SHOW_UPDATE_DIALOG = 0;
+	protected static final int URL_ERROR = 2;
+	protected static final int NETWORK_ERROR = 3;
+	protected static final int JSON_ERROR = 4;
+	
+	private TextView tv_splash_version;
+	
+	private Handler handler = new Handler(){
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+			Log.i(TAG, "升级检测结果: "+msg.what);
+			switch (msg.what ) {
+			case SHOW_UPDATE_DIALOG:
+				
+				break;
+			case ENTER_HOME:
+				enterHome();
+				break;
+			case URL_ERROR:
+				enterHome();
+				Toast.makeText(getApplicationContext(), "URL错误", Toast.LENGTH_SHORT).show();
+				break;
+			case NETWORK_ERROR:
+				enterHome();
+				Toast.makeText(getApplicationContext(), "网络异常", Toast.LENGTH_SHORT).show();
+				break;
+			case JSON_ERROR:
+				enterHome();
+				Toast.makeText(SplashActivity.this, "JSON解析出错", Toast.LENGTH_SHORT).show();
+				break;
+				
+			default:
+				break;
+			}
+			
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		
+		tv_splash_version = (TextView)findViewById(R.id.tv_splash_version);
+		tv_splash_version.setText(getString(R.string.appversion) + getVersionName());
+		
+		//检查升级
+		checkUpdate();
+		AlphaAnimation animation = new AlphaAnimation(0.2f	, 1.0f);
+		animation.setDuration(500);
+		findViewById(R.id.rl_root_splash).startAnimation(animation);
+		
+	}
+	
+	protected void enterHome() {
+		// TODO Auto-generated method stub
+		 Intent it = new Intent(this, HomeActivity.class);
+		 startActivity(it);
+		 //关闭当前页
+		 finish();
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash, menu);
-		return true;
+	/**
+	 *  检查新版本
+	 */
+	private void checkUpdate() {
+		// TODO Auto-generated method stub
+		
+		new Thread(){
+			public void run() {
+				//URL http://localhost:8080/updateinfo.json
+				Message mes = Message.obtain();
+				long startTime = System.currentTimeMillis();
+				try {
+					URL url = new URL(getString(R.string.serverurl));
+					//联网
+					HttpURLConnection con = (HttpURLConnection)url.openConnection();
+					con.setRequestMethod("GET");
+					con.setConnectTimeout(10000);
+					int code = con.getResponseCode();
+					if (code == 200) {
+						//联网成功
+						InputStream is = con.getInputStream();
+						//把流转成String
+						String result = StreamTools.readFromStream(is);
+						Log.i(TAG, "联网成功了 " + result);
+						//json解析
+						JSONObject obj = new JSONObject(result);
+						String apkurl = (String)obj.get("apkurl");
+						String  version = (String)obj.get("version");
+						String description = (String)obj.get("description");
+						
+						//校验是否有新版本
+						if (getVersionName().equals(version)) {
+							//版本一致，没有新版本，进入主页面
+							mes.what = ENTER_HOME;
+						}else {
+							//有新版本，弹出升级对话框
+							mes.what = SHOW_UPDATE_DIALOG;
+						}
+					}
+					
+				} catch (MalformedURLException e) {
+					// TODO Auto-generated catch block
+					mes.what = URL_ERROR;
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					mes.what = NETWORK_ERROR;
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					mes.what = JSON_ERROR;
+					e.printStackTrace();
+				}finally{
+						
+					long endTime = System.currentTimeMillis();
+					long dTime = endTime - startTime;	
+					if (dTime < 2000) {
+						try {
+							Thread.sleep(2000 - dTime);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					handler.sendMessage(mes);
+				}
+			};
+		}.start();
+		
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	/**
+	 * 得到app版本名称
+	 * */
+	private String getVersionName()
+	{
+		//管理手机apk
+		PackageManager pm = getPackageManager();
+
+		try {
+			//得到指定apk的功能清单文件
+			PackageInfo pInfo = pm.getPackageInfo(getPackageName(), 0);
+			return pInfo.versionName;
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
 		}
-		return super.onOptionsItemSelected(item);
 	}
+
 }
